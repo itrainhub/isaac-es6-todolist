b67304f5b15d8b86ef767022c84dec05b98c4f31
xiaoming <156246158@qq.com>
2020-03-17 15:55:14 +0800

    chore: 添加装饰器插件使用

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            plugins: [
+              ['@babel/plugin-proposal-decorators', {legacy: true}],
+              ['@babel/plugin-proposal-class-properties', {loose: true}]
+            ]
           }
         }
       }
