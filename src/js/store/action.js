c17974740243d1a394ce34faddbc7da3276a0f44
xiaoming <156246158@qq.com>
2020-03-17 17:23:43 +0800

    feat: 添加actionCreator

--- src/js/store/action.js
+++ src/js/store/action.js
@@ -3,3 +3,30 @@ export const ActionTypes = {
   UPDATE_TODO_ITEM: 'UPDATE_TODO_ITEM',
   DELETE_TODO_ITEM: 'DELETE_TODO_ITEM'
 }
+
+let uid = 1
+
+export const addTodoItemAction = todoItemText => dispatch => {
+  dispatch({
+    type: ActionTypes.ADD_TODO_ITEM,
+    payload: {
+      id: uid++,
+      title: todoItemText,
+      isCompleted: false
+    }
+  })
+}
+
+export const updateTodoItemAction = id => dispatch => {
+  dispatch({
+    type: ActionTypes.UPDATE_TODO_ITEM,
+    payload: { id }
+  })
+}
+
+export const deleteTodoItemAction = id => dispatch => {
+  dispatch({
+    type: ActionTypes.DELETE_TODO_ITEM,
+    payload: { id }
+  })
+}
